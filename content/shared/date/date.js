e5b2d5517381792cff9e2e6dbeaafd5bf6cd3c99
David Borgenvik <david.borgenvik@e-man.se>
2016-09-28 19:41:29 +0200

    debuging

--- content/shared/date/date.js
+++ content/shared/date/date.js
@@ -1,5 +1,6 @@
 console.log("in date");
-var el = document.currentScript.ownerDocument.getElementById("date")
+debugger;
+var el = document.currentScript.ownerDocument.getElementById("date");
 var date = new Date();
 el.innerHtml = date.toString();
 document.body.innerHtml = "bbbb";
