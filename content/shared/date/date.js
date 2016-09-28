8de608efbbc1968755ce2f86eee0494bd6d0fda1
David Borgenvik <david.borgenvik@e-man.se>
2016-09-28 19:48:57 +0200

    reverted everything.

--- content/shared/date/date.js
+++ content/shared/date/date.js
@@ -1,8 +1,5 @@
-setTimeout(
-    function() {
-        console.log("in date");
-        var el = document.getElementById("date");
-        var date = new Date();
-        el.innerHtml = date.toString();
-        document.body.innerHtml = "bbbb";
-  }, 10);
+console.log("in date");
+var el = document.currentScript.ownerDocument.getElementById("date");
+var date = new Date();
+el.innerHtml = date.toString();
+document.body.innerHtml = "bbbb";
