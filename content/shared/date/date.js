49bccbe1ba12d13f57276cca5a78ca9a97296895
David Borgenvik <david.borgenvik@e-man.se>
2016-09-28 19:47:56 +0200

    changed to document instead of ower document, and added timeout just to see if it would work.

--- content/shared/date/date.js
+++ content/shared/date/date.js
@@ -1,7 +1,7 @@
 setTimeout(
     function() {
         console.log("in date");
-        var el = document.currentScript.ownerDocument.getElementById("date");
+        var el = document.getElementById("date");
         var date = new Date();
         el.innerHtml = date.toString();
         document.body.innerHtml = "bbbb";
